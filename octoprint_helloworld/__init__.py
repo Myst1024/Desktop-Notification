c28f7578243065b598a9ddf9f09dbc3c3f478972
Gunnar Carlson <gunnar.carlson@gmail.com>
2019-03-17 22:22:46 -0500

    Basic JS execution

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -4,9 +4,15 @@ from __future__ import absolute_import
 import octoprint.plugin
 
 class DesktopNotificationPlugin(octoprint.plugin.StartupPlugin,
-		       octoprint.plugin.TemplatePlugin):
+		                        octoprint.plugin.TemplatePlugin,
+                                octoprint.plugin.AssetPlugin):
     def on_after_startup(self):
         self._logger.info("Enabled Desktop Notification Plugin!")
+
+    def get_assets(self):
+        return dict(
+            js=["js/notification.js"]
+        )
 
 __plugin_name__ = "Desktop Notification"
 __plugin_implementation__ = DesktopNotificationPlugin()
